d6f1907a5b0f6be8528b1555df28ace8be4bf0cc
xuy <3099817419@qq.com>
2021-06-28 10:04:21 +0800

    Squashed commit of the following:
    commit 9dec723e614c08ec753d7602a42c991a54b28bf5
    Author: xuy <3099817419@qq.com>
    Date:   Mon Jun 28 09:50:57 2021 +0800
    
        1
    
    commit d029429703ea401ea8c2987b5fa8a9c6a66cfcdc
    Author: Xuyic <309981749@qq.com>
    Date:   Sat Jun 26 23:25:21 2021 +0800
    
        小程序第二课，首页面定位，搜索
    
    commit 42f87dde14dd8fc0ca8acf34923a6bc5429afbc0
    Author: Xuyic <309981749@qq.com>
    Date:   Fri Jun 25 23:31:16 2021 +0800
    
        小程序第一课
    
    commit a237985bef7be1d5509870df661015eeb48ff211
    Author: Xuyic <309981749@qq.com>
    Date:   Fri Jun 25 20:22:53 2021 +0800
    
        删除无故添加的index
    
    commit a845832982a0edcff7226334aed409a0f51be3b0
    Merge: 71b87a3 597b0a0
    Author: Xuyic <309981749@qq.com>
    Date:   Fri Jun 25 20:17:06 2021 +0800
    
        Merge branch 'vx_dev' of github.com:zaqq147258369/vxmeituan into vx_dev
    
    commit 597b0a0126e66d3ba5bd5a951cb0804875f9802e
    Author: xuy <3099817419@qq.com>
    Date:   Fri Jun 25 10:17:30 2021 +0800
    
        测试
    
    commit 71b87a32bb1abe0592da4b738d0873428fb50152
    Author: Xuyic <309981749@qq.com>
    Date:   Thu Jun 24 22:43:11 2021 +0800
    
        微信小程序-学习美团外卖。基于uniapp开发

---
+++ pages/index/components/preference.vue
@@ -0,0 +1,22 @@
+<template>
+	<view>
+		为您优选
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
---
+++ pages/index/index.vue
@@ -0,0 +1,45 @@
+<template>
+	<view class="contenting">
+		<Serach></Serach>
+		<Preference></Preference>
+		<Title></Title>
+		<Delicacy></Delicacy>
+		<Takeout></Takeout>
+	</view>
+</template>
+
+<script>
+	//引进子组件
+	import Serach from './components/search.vue'
+	import Preference from './components/preference.vue'
+	import Title from './components/title.vue'
+	import Delicacy from './components/delicacy.vue'
+	import Takeout from './components/takeout.vue'
+	
+	export default {
+		components: {
+			Serach,
+			Preference,
+			Title,
+			Delicacy,
+			Takeout,
+		}, 
+		data() {
+			return {
+				title: 'Hello'
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style scoped="scoped">
+	.contenting{
+		margin: 0 15rpx;
+	}
+</style>
